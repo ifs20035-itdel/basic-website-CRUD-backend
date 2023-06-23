0bb64347f1d2d945ada51d2a8457263bbb930326
Michael Napitupulu <asm34686@gmail.com>
2023-06-23 22:48:29 +0700

    delete all structure

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const loginRoute = require('./api/routes/login')
+const loginRoute = require('./api/auth/login')
 const app = express();
 
 app.use('/auth/login', loginRoute);
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
-const app = require('./app')
+const app = require('./app');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
-
 const server = http.createServer(app);
+
+mongoose.connect(process.env.DATABSE_URL, { useNewUrlParser: true });
+const db = mongoose.connection
+
+db.on('error', error => console.error(error))
+db.once('open', error => console.log('Connected to Database'))
 
 server.listen(port);
